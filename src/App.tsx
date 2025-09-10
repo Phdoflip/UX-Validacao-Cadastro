d95f5d983319ba0a912033b7a078f60ab3d15efc
Phdoflip <antrodossantos@gmail.com>
2025-09-10 09:38:19 -0300

    Cadastro limpo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+    confirmarSenha: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log("Dados enviados:", formData);
+  }
 
   return (
-    <>
-      
-      
-    </>
-  )
+    <div className="container">
+      <h1>Cadastro</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <label>
+          Nome:
+          <input
+            type="text"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            placeholder="Digite seu nome"
+          />
+        </label>
+
+        <label>
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Digite seu e-mail"
+          />
+        </label>
+
+        <label>
+          Senha:
+          <input
+            type="password"
+            name="senha"
+            value={formData.senha}
+            onChange={handleChange}
+            placeholder="Digite sua senha"
+          />
+        </label>
+
+        <label>
+          Confirmar senha:
+          <input
+            type="password"
+            name="confirmarSenha"
+            value={formData.confirmarSenha}
+            onChange={handleChange}
+            placeholder="Confirme sua senha"
+          />
+        </label>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
